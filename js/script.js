5c3bdee572e27d18d25c0b4f6e2c3e855f73f863
Melvin <mmasdieu@gpartner.eu>
2015-02-26 16:59:47 +0100

    connexion js

--- js/script.js
+++ js/script.js
@@ -13,12 +13,33 @@ $(document).ready(function () {
                 'login': login,
                 'password': password
             },
-            success: function (response) {
-                alert(response);
-                var href = $('#details-user');
-                href.text('deconnexion');
-                href.attr('href', "onclick='deconnexion()'");
-                href.attr('alt', 'deconnexion');
+            success: function (data) {
+                if(data == 1) {
+                    $.ajax({
+                        url: '/index.php',
+                        type: 'POST',
+                        data: {
+                            'action': 'load',
+                            'pageId': 4
+                        },
+                        success: function (response) {
+                            document.getElementById('content').innerHTML = response;
+                            var href = $('#details-user');
+                            href.text('deconnexion');
+                            href.attr('href', "onclick='deconnexion()'");
+                            href.attr('alt', 'deconnexion');
+                        }
+                    });
+                }
+                else {
+                    $.noty.defaults = {
+                        layout: 'BottomRight',
+                        theme: 'relax', // or 'relax'
+                        type: 'warning',
+                        text: 'Erreur de connexion, login ou mot de passe incorrect !',
+                        timeout: 500
+                    };
+                }
             }
         });
     });
